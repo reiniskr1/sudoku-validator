b8389a7bd480c66895e5a82fab17eb381a03e10c
reiniskr1 <Reinis.Krauja@gmail.com>
2023-05-05 21:12:08 +0300

    Reads the given String to a 2d Array

--- src/validator.js
+++ src/validator.js
@@ -6,8 +6,27 @@ class Validator {
   }
 
   validate(sudoku) {
+    let data = readToArray(sudoku);
     // Your code here
   }
 }
 
+function readToArray(SudokuString){
+  let SudokuArr = [];
+  let RowArr = [];
+  let currentChar;
+  for (let i = 0; i < SudokuString.length; i++){
+    currentChar = SudokuString.charAt(i);
+    if (currentChar === '|' || currentChar === '+' || currentChar === '-' || currentChar === ' ') continue;
+    if (currentChar === '\n'){
+      SudokuArr.push(RowArr);
+      RowArr = [];
+      continue
+    }
+    RowArr.push(parseInt(currentChar));
+  }
+  if (RowArr.length !== 0)SudokuArr.push(RowArr);
+  return SudokuArr;
+
+}
 module.exports = Validator
